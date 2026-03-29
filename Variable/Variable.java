657c3d62e3c127e5925f577aca5a5a118f42859a
Priyansh <Priyanshyadav1012@gmail.com>
2026-03-29 11:42:27 +0530

    Variable

---
+++ Variable/Variable.java
@@ -0,0 +1,12 @@
+package Variable;
+
+public class Variable {
+    public static void main(String[] args) {
+        int age = 25;
+        String name = "Priyansh";
+        long marks = 90L;
+        float percentage = 90.0f;
+        
+        System.out.println(name + " is " + age + " years old and scored " + marks + " marks which is equivalent to " + percentage + "%.");
+    }
+}
